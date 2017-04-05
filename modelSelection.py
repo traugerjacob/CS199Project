e5af16b58d274dee496b7b0e55108d21fa0d954d
Jacob Trauger <jtt2@illinois.edu>
2017-04-05 11:43:46 -0500

    Implemented scikit learns cheat sheet as a starting point

--- modelSelection.py
+++ modelSelection.py
@@ -6,21 +6,44 @@ sc = SparkContext(conf = conf)
 def main(argv):
 		#This is a lot of parameters needed for the model selection process. If there are any that can be removed talk to partner about it to see if it can be done
 	if len(argv) < 5:
-		print("The arguements for this script require(if there is a space in the name of a parameter please replace the space with a +):\npath/to/filename of the dataset\nsupervised/unsupervised\nclassifier/regression\nparameter trying to be guessed\nother parameters")
+#idk if there is an unsupervised regression algorithm so if there is then add itand change the code around to accomidate it
+		print("The arguements for this script require(if there is a space in the name of a parameter please replace the space with a +):\npath/to/filename of the dataset\nsupervised/unsupervised\nclassifier/regression/clustering\nparameter trying to be guessed\nother parameters")
 	else:
 		args = argv[1:]
 		dataset = sc.textFile(args[0])
-		if(args[2] == "classification"):
-			#code goes here to find model selection for classification
-			print(-1)
-			return -1 #delete this when code is written
-		if(args[2] == "regression"):
-			#code goes here to find model selection for regression
-			print(-1)
-			return -1 #delete this when code is written
-		else:
-			print("Please use rather classification or regression")
-			return;
+		if(args[1] == "supervised"):
+			if(args[2] == "classification"):
+				#code goes here to find model selection for classification
+				if(count(dataset) > 100000):
+					text = ""
+					while(text !="y" or text !="n"):
+						text = input("Text data? y/n")
+					if(text = "y")
+						return "Naive Bayes"
+					else:
+						return "KNeighbors Classifier and if that doesnt work then SVC or Ensemble Classifiers"
+
+			if(args[2] == "regression"):
+				#code goes here to find model selection for regression
+				if(count(dataset) > 100000):
+					if (len(args) < 6):
+						return "Lasso"
+					else:
+						return "Ridge Regression/SVR and then Ensemble regressors"
+			else:
+				print("Please use rather classification or regression")
+				return;
+		if(args[1] == "unsupervised"):
+			if(args[2]) == "clustering":
+				text = ""
+				while(text !="y" or text !="n"):
+					text = input("Text data? y/n")
+				if(text = "y")
+					return "Latent Dirichlet Allocation"
+				else:
+					return "KMeans and if that does not work than Guassian Mixture Modeling"
+			else:
+				print("please use clusetering")
 
 
 
