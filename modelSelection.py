5858fcaf0e804339b265ef057241058e4a3673a2
Jacob Trauger <jtt2@illinois.edu>
2017-04-10 18:27:09 -0500

    wrote what happens for clustering and added TODO's to get a general idea of what is left to do

--- modelSelection.py
+++ modelSelection.py
@@ -1,17 +1,21 @@
+#TODO find a way to filter the dataset to only get the parameters we want left in them
+#TODO split the dataset into training and testing data (also when finding the best model should we train on a smaller dataset to pick the best model and then use that model on the full dataset?)
+#TODO fill in the stubbed functions for the models
+
 from pyspark import SparkContext, SparkConf
 import sys
 conf = SparkConf().setAppName("Model Selection")
 sc = SparkContext(conf = conf)
 
 
-def checkParams(row):
+def checkParams(row, params):
 	for i in row:
-		if i == None:
+		if i == None or i in params:
 			return False
 	return True
 
 def filtered(data, params):
-	data = data.filter(lambda x: checkParams(x))
+	data = data.filter(lambda x: checkParams(x, params))
 	return data
 
 
@@ -29,6 +33,10 @@ def performRandomForest(data, params):
 
 #returns the best model for the data given the parameters
 def performClassification(data, params):
+	from pyspark.mllib.regression import LabeledPoint
+	from pyspark.mllib.classification import NaiveBayes
+	from pyspark.mllib.evaluation import MulticlassMetrics
+
 	naiveBayes = performNaiveBayes(data, params)
 	randomForest = performRandomForest(data, params)
 	return None
@@ -64,26 +72,40 @@ def performRegression(data, params):
 
 
 #returns the K-Means model
-def performKMeans(data, params):
-	return None
+def performKMeans(data, k):
+	kMeans = KMeans.train(data, k)
 
 
 #reutrns the Guassian Mixture model
-def performGaussianMixture(data, params):
-	return None
+def performGaussianMixture(data, k):
+	gmm = GaussianMixture.train(data, k)
+	return gmm
 
 #returns the best clustering model for the dataset given the parameters
 def performClustering(data, params):
-	kMeans = performKMeans(data, params)
-	guassian = performGuassianMixture(data, params)
-	return None
+	from pyspark.mllib.clustering import KMeans, KMeansModel
+	from pyspark.mllib.clustering import GaussianMixture, GaussianMixtureModel
+	
+	#Make sure to find the best k value.
+	bestKMeans = 2
+	bestKGaussian = 2
+	kMeansBest = -1
+	guassianBest = -1
+	for k in range(2,20):
+		kMeans = performKMeans(data, k)
+		guassian = performGuassianMixture(data, k)
+		#TODO do some computations to see if this k is best (it isnt going to be the best value but where right before where the dimishing returns is too great)
+		#And see if KMeans or guassian does better with each of their respective best k values
+	if guassian < kMeans:
+		return ("guassian", bestKGuassian)
+	else:
+		return ("kMeans", bestKMeans)
+	
 
 
 def main(argv):
-		#This is a lot of parameters needed for the model selection process. If there are any that can be removed talk to partner about it to see if it can be done
 	if len(argv) < 5:
-		#idk if there is an unsupervised regression algorithm so if there is then add itand change the code around to accomidate it
-		print("The arguements for this script require(replace sapce in parameters with a '+'):\n" +
+		print("The arguments for this script require:\n" +
 				"path/to/filename of the dataset\n" +
 				"supervised/unsupervised\n" +
 				"classifier/regression/clustering\n" +
@@ -94,36 +116,45 @@ def main(argv):
 
 		#sets up the RDD
 		dataset = sc.textFile(args[0])
-		if(args[-3:] == "csv"):
+		if args[-3:] == "csv":
 			import csv
 			dataset = dataset.mapPartitions(lambda x: csv.reader(x))
 
-		elif(args[-4:] =="json"):
+		elif args[-4:] =="json":
 			import json
 			dataset = dataset.map(json.loads)
 
 		params = argv[3:]
 		
-		#filters dataset to get all None values out
+		#filters dataset to get all None/header values out
 		dataset = filtered(dataset, params)
 		
 		#Model selection algorithm. Currently goes off of scikit learn's cheat sheet
-		if(args[1] == "supervised"):
-			if(args[2] == "classification"):
+		if args[1] == "supervised":
+			if args[2] == "classification":
 				model = performClassification(dataset, params)
 				#TODO predict the model across the entire dataset
-			if(args[2] == "regression"):
+
+			if args[2] == "regression":
 				model = performRegression(dataset, params)		
 				#TODO predict the model across the entire dataset
+			
 			else:
 				print("Please use rather classification or regression for supervised learning")
 				return
 
-		if(args[1] == "unsupervised"):
-			if(args[2]) == "clustering":
-				model = perfromClustering(dataset, params)		
-				#TODO predict the model across the entire dataset
+		if args[1] == "unsupervised":
+			if args[2] == "clustering":
+				model = perfromClustering(dataset, params)
+				if(model[0] == "gaussian"):
+					theModel = GuassianMixture.train(datasetTraining, model[1])
+				else:
+					theModel = KMeans.train(datasetTraining, model[1])
+
+				return theModel
 			else:
 				print("Currently this model selection algorithm only supports clustering for unsupervised algorithms")
 				return
+
+
 main(sys.argv)
